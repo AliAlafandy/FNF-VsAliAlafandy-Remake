d1072e801d27e3d429e50e36abef5eba2a0d95ca
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2025-12-07 14:07:20 +0200

    Update ResetScoreSubState.hx

--- source/substates/ResetScoreSubState.hx
+++ source/substates/ResetScoreSubState.hx
@@ -73,7 +73,7 @@ class ResetScoreSubState extends MusicBeatSubstate
 		add(yesText);
 		
 		for(letter in yesText.letters)
-			letter.color = FlxColor.GREEN;
+			letter.color = FlxColor.RED;
 		
 		noText = new Alphabet(0, text.y + 150, 'No', true);
 		noText.screenCenter(X);
